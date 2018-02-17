5b065cfe9326f7fbb211775bb57b4202309b9b86
Vlad Bibire <VladBibire@Vlads-MacBook-Pro.local>
2018-02-17 17:49:21 +0000

    Fixed Nav Bug on Blog

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,9 +1,11 @@
 // Side Nav
 function openSideMenu(){
-  document.getElementById('side-menu').style.width = '100%';
+  document.getElementById('side-menu').style.width = '90%';
+  document.body.style.overflowY = "hidden";
 }
 function closeSideMenu(){
   document.getElementById('side-menu').style.width = '0';
+  document.body.style.overflowY = "visible";
 }
 
 // Nav Fixed Top
@@ -13,6 +15,7 @@ var sticky = navigation.offsetTop;
 function fixedNav(){
   if (window.pageYOffset >= sticky){
     navigation.classList.add('fixed');
+    document.getElementById('blog-header').style.marginTop = '50px';
   } else {
     navigation.classList.remove('fixed');
   }
